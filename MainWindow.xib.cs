2d3d6376f2ecde044a3289ef75763e06e3cd44f6
ConceptDev <craig.dunn@gmail.com>
2010-05-01 20:30:33 +1000

    Convert to iPad/Universal App

--- MainWindow.xib.cs
+++ MainWindow.xib.cs
@@ -3,7 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Drawing;
-using System.IO; // for Path
+using System.IO;
+// for Path
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
@@ -12,15 +13,16 @@ namespace iSOFlair
 	// The name AppDelegate is referenced in the MainWindow.xib file.
 	public partial class AppDelegate : UIApplicationDelegate
 	{
-		List<TrilogySite> list = TrilogySite.GetAll();
-		int siteCount = 0; // TODO: remove 'hardcoded' fixed number of four sites
-		int maxPageVisited = 0; // used to set the 'badge' number: number of un-viewed panels
-		
+		List<TrilogySite> list = TrilogySite.GetAll ();
+		int siteCount = 0;
+		// TODO: remove 'hardcoded' fixed number of four sites
+		int maxPageVisited = 0;
+		// used to set the 'badge' number: number of un-viewed panels
 		/// <summary>
 		/// http://wiki.monotouch.net/HowTo/Files/HowTo%3a_Store_Files
 		/// </summary>
-		string documentsDirectory="";
-		
+		string documentsDirectory = "";
+
 		/// <summary>
 		/// When shutting down, set the badge number to 'unviewed' panel count
 		/// </summary>
@@ -32,43 +34,47 @@ namespace iSOFlair
 		// This method is invoked when the application has loaded its UI and its ready to run
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
+			window.Frame = UIScreen.MainScreen.Bounds;
+
 			documentsDirectory = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
-			documentsDirectory = documentsDirectory.Replace("Documents", "tmp"); // use tmp dir, don't need iTunes to back-up
-			
+			documentsDirectory = documentsDirectory.Replace ("Documents", "tmp");
+			// use tmp dir, don't need iTunes to back-up
 			// load preferences in a  loop
 			var prefs = NSUserDefaults.StandardUserDefaults;
 			foreach (var site in list)
 			{
-				site.SiteId = prefs.StringForKey(site.PreferencesPrefix + "id");
+				site.SiteId = prefs.StringForKey (site.PreferencesPrefix + "id");
 				siteCount++;
-				Console.WriteLine(site.PreferencesPrefix + "id" + ": " + site.SiteId);	
+				Console.WriteLine (site.PreferencesPrefix + "id" + ": " + site.SiteId);
 			}
 			UIApplication.SharedApplication.ApplicationSupportsShakeToEdit = true;
 			
-			CreatePanels();
+			CreatePanels ();
 			
 			// unviewed panel count starts at 4, and is decremented when view is changed
 			// by a swipe or pagercontrol touch
 			UIApplication.SharedApplication.ApplicationIconBadgeNumber = 4;
 			
 			// handler for when the user navigates via the pager, rather than swiping
-			pageControl.ValueChanged += delegate(object sender, EventArgs e) {
+			pageControl.ValueChanged += delegate(object sender, EventArgs e)
+			{
 				var pc = (UIPageControl)sender;
-				double fromPage = Math.Floor((scrollView.ContentOffset.X - scrollView.Frame.Width / 2) / scrollView.Frame.Width) + 1;
+				double fromPage = Math.Floor ((scrollView.ContentOffset.X - scrollView.Frame.Width / 2) / scrollView.Frame.Width) + 1;
 				var toPage = pc.CurrentPage;
 				var pageOffset = scrollView.ContentOffset.X + scrollView.Frame.Width;
-				Console.WriteLine("fromPage " + fromPage + " toPage " + toPage);	
+				Console.WriteLine ("fromPage " + fromPage + " toPage " + toPage);
 				if (fromPage > toPage)
 					pageOffset = scrollView.ContentOffset.X - scrollView.Frame.Width;
-				PointF p = new PointF(pageOffset, 0);
-				scrollView.SetContentOffset(p,true);
-				list[toPage].ViewController.BecomeFirstResponder(); // so it can "accept" shakes
+				PointF p = new PointF (pageOffset, 0);
+				scrollView.SetContentOffset (p, true);
+				list[toPage].ViewController.BecomeFirstResponder ();
+				// so it can "accept" shakes
 				// so we can change the 'badge' on the application icon
-				maxPageVisited = maxPageVisited<toPage?toPage:maxPageVisited;
+				maxPageVisited = maxPageVisited < toPage ? toPage : maxPageVisited;
 			};
 			
 			window.MakeKeyAndVisible ();
-
+			
 			return true;
 		}
 
@@ -77,7 +83,7 @@ namespace iSOFlair
 		public override void OnActivated (UIApplication application)
 		{
 		}
-		
+
 		/// <summary>
 		/// Create an SOViewController for each 'site'
 		/// </summary>
@@ -91,38 +97,39 @@ namespace iSOFlair
 		/// call BecomeFirstResponder when we change views... this is because
 		/// the 'shake gesture' goes to the FirstResponder
 		/// </remarks>
-		private void CreatePanels()
+		private void CreatePanels ()
 		{
-		    scrollView.Scrolled += ScrollViewScrolled;
+			scrollView.Scrolled += ScrollViewScrolled;
 			
-		    int count = siteCount;
-		    RectangleF scrollFrame = scrollView.Frame;
-		    scrollFrame.Width = scrollFrame.Width * count;
-		    scrollView.ContentSize = scrollFrame.Size;
-		
+			int count = siteCount;
+			RectangleF scrollFrame = scrollView.Frame;
+			scrollFrame.Width = scrollFrame.Width * count;
+			scrollView.ContentSize = scrollFrame.Size;
+			
 			SOViewController defaultFirstResponderView = null;
-		    for (int i=0; i<count; i++)
-		    {
-		        RectangleF frame = scrollView.Frame;
-		        PointF location = new PointF();
-		        location.X = frame.Width * i;
-		
-		        frame.Location = location;
-		        
-				SOViewController cellController = new SOViewController();
-				NSBundle.MainBundle.LoadNib("SOViewController", cellController, null);
+			for (int i = 0; i < count; i++) {
+				RectangleF frame = scrollView.Frame;
+				PointF location = new PointF ();
+				location.X = frame.Width * i;
+				
+				frame.Location = location;
+				
+				SOViewController cellController = new SOViewController ();
+				NSBundle.MainBundle.LoadNib ("SOViewController", cellController, null);
 				cellController.View.Frame = frame;
 				cellController.Site = list[i];
 				cellController.DocumentsDirectory = documentsDirectory;
-				cellController.WireUp();
-				cellController.LoadFromCache();
+				cellController.WireUp ();
+				cellController.LoadFromCache ();
 				
 				list[i].ViewController = cellController;
-				scrollView.AddSubview(cellController.View);
-				if (i==0) defaultFirstResponderView = cellController; // set the first one to FirstResponder
-		    }
-		    pageControl.Pages = count;
-			defaultFirstResponderView.BecomeFirstResponder();
+				scrollView.AddSubview (cellController.View);
+				if (i == 0)
+					defaultFirstResponderView = cellController;
+				// set the first one to FirstResponder
+			}
+			pageControl.Pages = count;
+			defaultFirstResponderView.BecomeFirstResponder ();
 		}
 
 		/// <summary>
@@ -131,20 +138,18 @@ namespace iSOFlair
 		/// </summary>
 		private void ScrollViewScrolled (object sender, EventArgs e)
 		{
-			Console.Write("Swiped from " + pageControl.CurrentPage);
-		    double page = Math.Floor((scrollView.ContentOffset.X - scrollView.Frame.Width / 2) / scrollView.Frame.Width) + 1;
-		    int pageNumber = (int)page;
-			if (pageNumber >= 0 && pageNumber <list.Count)
-			{
+			Console.Write ("Swiped from " + pageControl.CurrentPage);
+			double page = Math.Floor ((scrollView.ContentOffset.X - scrollView.Frame.Width / 2) / scrollView.Frame.Width) + 1;
+			int pageNumber = (int)page;
+			if (pageNumber >= 0 && pageNumber < list.Count) {
 				pageControl.CurrentPage = pageNumber;
-				Console.WriteLine(" to " + pageControl.CurrentPage);
-				list[(int)page].ViewController.BecomeFirstResponder(); // so it can "accept" shakes
+				Console.WriteLine (" to " + pageControl.CurrentPage);
+				list[(int)page].ViewController.BecomeFirstResponder ();
+				// so it can "accept" shakes
 				// so we can change the 'badge' on the application icon
-				maxPageVisited = maxPageVisited<pageControl.CurrentPage?pageControl.CurrentPage:maxPageVisited;
-			}
-			else
-			{
-				Console.WriteLine("Scrolled a little too far, to page " + page);
+				maxPageVisited = maxPageVisited < pageControl.CurrentPage ? pageControl.CurrentPage : maxPageVisited;
+			} else {
+				Console.WriteLine ("Scrolled a little too far, to page " + page);
 			}
 		}
 	}
--- SOViewController.xib.cs
+++ SOViewController.xib.cs
@@ -40,43 +40,37 @@ namespace iSOFlair
 		{
 			
 		}
-		
+
 		#endregion
-		
+
 		private TrilogySite _site = null;
-		
+
 		public TrilogySite Site {
-			get{ return _site;}
-			set
-			{
+			get { return _site; }
+			set {
 				_site = value;
 				
-				imageLogo.Image = UIImage.FromFile(Site.Logo);
+				imageLogo.Image = UIImage.FromFile (Site.Logo);
 			}
 		}
 		/// <summary>
 		/// Path to save image and cache
 		/// </summary>
-		public string DocumentsDirectory{get;set;}
-		
+		public string DocumentsDirectory { get; set; }
+
 		public string Username {
-			get {return labelUsername.Text;}
-			set {labelUsername.Text = value;}
+			get { return labelUsername.Text; }
+			set { labelUsername.Text = value; }
 		}
-		public string Points
-		{
-			get {return labelPoints.Text;}
-			set {labelPoints.Text = value;}
+		public string Points {
+			get { return labelPoints.Text; }
+			set { labelPoints.Text = value; }
 		}
-		private string ImagePath{
-			get {
-				return Path.Combine (DocumentsDirectory, Site.PreferencesPrefix+"gravatar.png");
-			}
+		private string ImagePath {
+			get { return Path.Combine (DocumentsDirectory, Site.PreferencesPrefix + "gravatar.png"); }
 		}
-		private string CachePath{
-			get {
-				return Path.Combine (DocumentsDirectory, Site.PreferencesPrefix+"cache.txt");
-			}
+		private string CachePath {
+			get { return Path.Combine (DocumentsDirectory, Site.PreferencesPrefix + "cache.txt"); }
 		}
 		/// <summary>
 		/// Setup the button delegates
@@ -86,289 +80,244 @@ namespace iSOFlair
 			buttonUpdate.TouchDown += delegate {
 				buttonUpdate.Hidden = true;
 				buttonSafari.Hidden = true;
-				ScreenCapture();
+				ScreenCapture ();
 				buttonUpdate.Hidden = false;
 				buttonSafari.Hidden = false;
 			};
-			buttonSafari.TouchDown += delegate{
-				UIApplication.SharedApplication.OpenUrl(new NSUrl(String.Format(Site.ProfileUrl 
-			                                , Site.SiteId)));
-			};
+			buttonSafari.TouchDown += delegate { UIApplication.SharedApplication.OpenUrl (new NSUrl (String.Format (Site.ProfileUrl, Site.SiteId))); };
 		}
-		
+
 		#region respond to shaking (OS3+)
 		// also requires you to put
 		// UIApplication.SharedApplication.ApplicationSupportsShakeToEdit = true;
 		// in Main.cs : FinishedLaunching()
 		public override bool CanBecomeFirstResponder {
-			get {
-				return true;
-			}
+			get { return true; }
 		}
 		public override void ViewDidAppear (bool animated)
 		{
 			base.ViewDidAppear (animated);
-			this.BecomeFirstResponder();
+			this.BecomeFirstResponder ();
 		}
 		public override void ViewWillDisappear (bool animated)
 		{
-			this.ResignFirstResponder();
+			this.ResignFirstResponder ();
 			base.ViewWillDisappear (animated);
 		}
 		public override void MotionEnded (UIEventSubtype motion, UIEvent evt)
 		{
-			Console.WriteLine("Motion detected");
-			if (motion ==  UIEventSubtype.MotionShake)
-			{
-				Console.WriteLine("and was a shake");
-				labelLastUpdated.Text = "All shook up! Updating..."; // never appears
+			Console.WriteLine ("Motion detected");
+			if (motion == UIEventSubtype.MotionShake) {
+				Console.WriteLine ("and was a shake");
+				labelLastUpdated.Text = "All shook up! Updating...";
+				// never appears
 				// Do your application-specific shake response here...
-				Update();			
+				Update ();
 			}
 		}
 		#endregion
 		/// <summary>
 		/// Load data from txt file (cached from last webclient request)
 		/// </summary>
-		public bool LoadFromCache()
+		public bool LoadFromCache ()
 		{
 			bool loaded = false;
-			Console.WriteLine("Loading from cache.txt");
+			Console.WriteLine ("Loading from cache.txt");
 			try {
 				string cache = "";
-	        		if (File.Exists(CachePath))
-				{
-					cache = System.IO.File.ReadAllText(CachePath);
+				if (File.Exists (CachePath)) {
+					cache = System.IO.File.ReadAllText (CachePath);
 				}
-				if (cache == "")
-				{
-					labelConnectionError.Text ="Cache is empty. Shake to refresh data.";
+				if (cache == "") {
+					labelConnectionError.Text = "Cache is empty. Shake to refresh data.";
 					labelUsername.Text = "";
-					labelPoints.Text="";
+					labelPoints.Text = "";
 					labelGoldBadges.Text = "";
-					labelSilverBadges.Text="";
-					labelBronzeBadges.Text="";
-					textDebug.Text ="";
-				}
-				else
-				{
-					string[] cacheItems = cache.Split('?');
+					labelSilverBadges.Text = "";
+					labelBronzeBadges.Text = "";
+					textDebug.Text = "";
+				} else {
+					string[] cacheItems = cache.Split ('?');
 					labelUsername.Text = cacheItems[0];
 					labelPoints.Text = cacheItems[1];
 					labelGoldBadges.Text = cacheItems[2] + " gold badges";
 					labelSilverBadges.Text = cacheItems[3] + " silver badges";
 					labelBronzeBadges.Text = cacheItems[4] + " bronze badges";
 					//profileUrl = cacheItems[5];
-					labelLastUpdated.Text=cacheItems[6] + " from cache";
-					textDebug.Text =""; // blank out, nothing loaded from the web
-					
-					if (File.Exists(ImagePath))
-					{
-						UIImage img = UIImage.FromFileUncached(ImagePath); // TODO: WHY didn't FromFile work, when FromFileUncached does?
+					labelLastUpdated.Text = cacheItems[6] + " from cache";
+					textDebug.Text = "";
+					// blank out, nothing loaded from the web
+					if (File.Exists (ImagePath)) {
+						UIImage img = UIImage.FromFileUncached (ImagePath);
+						// TODO: WHY didn't FromFile work, when FromFileUncached does?
 						if (img != null)
 							this.imageAvatar.Image = img;
 					}
 					loaded = true;
 				}
-			} 
-			catch (Exception ex)
-			{
-				labelConnectionError.Text ="Cache is invalid. Shake to refresh data.";
+			} catch (Exception ex) {
+				labelConnectionError.Text = "Cache is invalid. Shake to refresh data.";
 				labelUsername.Text = "";
-				labelPoints.Text="";
+				labelPoints.Text = "";
 				labelGoldBadges.Text = "";
-				labelSilverBadges.Text="";
-				labelBronzeBadges.Text="";
-				textDebug.Text ="";
+				labelSilverBadges.Text = "";
+				labelBronzeBadges.Text = "";
+				textDebug.Text = "";
 			}
 			return loaded;
 		}
-		
+
 		/// <summary>
 		/// Update this site data from the web, but downloading the flair, parsing 
 		/// and saving in a cache text file
 		/// </summary>
-		public void Update()
+		public void Update ()
 		{
-			Console.WriteLine("Loading from web " + Site.SiteId);
+			Console.WriteLine ("Loading from web " + Site.SiteId);
 			labelLastUpdated.Text = "Loading...";
 			
 			// Download flair page
-			WebClient wc = new WebClient();
-			Uri uri = new Uri(String.Format(Site.FlairUrl, Site.SiteId));
+			WebClient wc = new WebClient ();
+			Uri uri = new Uri (String.Format (Site.FlairUrl, Site.SiteId));
 			byte[] bytes = null;
-			try
-			{
+			try {
 				UIApplication.SharedApplication.NetworkActivityIndicatorVisible = true;
-				bytes = wc.DownloadData(uri);
-			} 
-			catch (Exception ex)
-			{
-				Console.WriteLine("Internet connection failed: " + ex.Message);
-				textDebug.Text = Environment.NewLine +"DOWNLOADDATA:"
-					+Environment.NewLine+ ex.Message;
+				bytes = wc.DownloadData (uri);
+			} catch (Exception ex) {
+				Console.WriteLine ("Internet connection failed: " + ex.Message);
+				textDebug.Text = Environment.NewLine + "DOWNLOADDATA:" + Environment.NewLine + ex.Message;
 				labelLastUpdated.Text = "Internet connection failed: " + ex.Message;
 				labelConnectionError.Text = "Update failed: cannot connect to the Internet.";
 				return;
-			} 
-			finally
-			{
+			} finally {
 				UIApplication.SharedApplication.NetworkActivityIndicatorVisible = false;
 			}
 			// Parse flair HTML
-			try 
-			{
-				string result = Encoding.UTF8.GetString(bytes);
-					
-				int startpos = result.IndexOf("<a");
-				startpos = result.IndexOf("href=",startpos) + 6;
-				int endpos = result.IndexOf("\"",startpos);
-				string profileUrl = result.Substring(startpos, endpos-startpos);
+			try {
+				string result = Encoding.UTF8.GetString (bytes);
 				
-				startpos = result.IndexOf("img src",endpos) + 9;
-				endpos = result.IndexOf("\"",startpos);
+				int startpos = result.IndexOf ("<a");
+				startpos = result.IndexOf ("href=", startpos) + 6;
+				int endpos = result.IndexOf ("\"", startpos);
+				string profileUrl = result.Substring (startpos, endpos - startpos);
+				
+				startpos = result.IndexOf ("img src", endpos) + 9;
+				endpos = result.IndexOf ("\"", startpos);
 				
 				// download image
-				string gravatarUrl = result.Substring(startpos, endpos-startpos);
-				uri = new Uri(gravatarUrl);
-				try
-				{	
-					Console.WriteLine("wc.Download("+uri+","+ImagePath+")");
+				string gravatarUrl = result.Substring (startpos, endpos - startpos);
+				uri = new Uri (gravatarUrl);
+				try {
+					Console.WriteLine ("wc.Download(" + uri + "," + ImagePath + ")");
 					wc.DownloadFile (uri, ImagePath);
-				} 
-				catch (Exception ex1)
-				{
-					textDebug.Text += Environment.NewLine + "DOWNLOAD IMAGE:"
-						+ Environment.NewLine + gravatarUrl
-						+ Environment.NewLine + ImagePath
-						+ Environment.NewLine + ex1.Message;
+				} catch (Exception ex1) {
+					textDebug.Text += Environment.NewLine + "DOWNLOAD IMAGE:" + Environment.NewLine + gravatarUrl + Environment.NewLine + ImagePath + Environment.NewLine + ex1.Message;
 				}
 				
 				// Parse the HTML
 				//HACK: yes this is a hack... HtmlAgilityPack would've been smarter.
-				startpos = result.IndexOf("username",endpos);
-				startpos = result.IndexOf("_blank",endpos) + 8;
-				endpos = result.IndexOf("<",startpos);
+				startpos = result.IndexOf ("username", endpos);
+				startpos = result.IndexOf ("_blank", endpos) + 8;
+				endpos = result.IndexOf ("<", startpos);
 				
-				string username = result.Substring(startpos, endpos-startpos);
+				string username = result.Substring (startpos, endpos - startpos);
 				
 				labelUsername.Text = username;
 				
-				startpos = result.IndexOf("reputation score",endpos) + 18;
-				endpos = result.IndexOf("<",startpos);
+				startpos = result.IndexOf ("reputation score", endpos) + 18;
+				endpos = result.IndexOf ("<", startpos);
 				
-				string points = result.Substring(startpos, endpos-startpos);
-				string goldBadges="", silverBadges="", bronzeBadges="";
-				startpos = result.IndexOf("gold badge",endpos);
-				if (startpos > 0)
-				{
-					startpos --; // remove preceding space
-					endpos = result.IndexOf("\"",startpos-10) + 1;
-					goldBadges = result.Substring(endpos, startpos-endpos);
-					Console.WriteLine(" " + goldBadges);
+				string points = result.Substring (startpos, endpos - startpos);
+				string goldBadges = "", silverBadges = "", bronzeBadges = "";
+				startpos = result.IndexOf ("gold badge", endpos);
+				if (startpos > 0) {
+					startpos--;
+					// remove preceding space
+					endpos = result.IndexOf ("\"", startpos - 10) + 1;
+					goldBadges = result.Substring (endpos, startpos - endpos);
+					Console.WriteLine (" " + goldBadges);
 					labelGoldBadges.Text = goldBadges + " gold badges";
 				}
-				startpos = result.IndexOf("silver badge",endpos);
-				if (startpos > 0)
-				{
-					startpos --; // remove preceding space
-					endpos = result.IndexOf("\"",startpos-10) + 1;
-					silverBadges = result.Substring(endpos, startpos-endpos);
+				startpos = result.IndexOf ("silver badge", endpos);
+				if (startpos > 0) {
+					startpos--;
+					// remove preceding space
+					endpos = result.IndexOf ("\"", startpos - 10) + 1;
+					silverBadges = result.Substring (endpos, startpos - endpos);
 					labelSilverBadges.Text = silverBadges + " silver badges";
-				}			
-				startpos = result.IndexOf("bronze badge",endpos);
-				if (startpos > 0)
-				{
-					startpos --; // remove preceding space
-					endpos = result.IndexOf("\"",startpos-10) + 1;
-					bronzeBadges = result.Substring(endpos, startpos-endpos);
+				}
+				startpos = result.IndexOf ("bronze badge", endpos);
+				if (startpos > 0) {
+					startpos--;
+					// remove preceding space
+					endpos = result.IndexOf ("\"", startpos - 10) + 1;
+					bronzeBadges = result.Substring (endpos, startpos - endpos);
 					labelBronzeBadges.Text = bronzeBadges + " bronze badges";
-				}	
+				}
 				// load image from file into UIImage
-				try
-				{
-					if (File.Exists(ImagePath))
-					{
+				try {
+					if (File.Exists (ImagePath)) {
 						//UIImage img = UIImage.FromFile (ImagePath); // WHY does this break???
-						UIImage img = UIImage.FromFileUncached(ImagePath); // BUT this works???
+						UIImage img = UIImage.FromFileUncached (ImagePath);
+						// BUT this works???
 						if (img != null)
 							this.imageAvatar.Image = img;
+					} else {
+						textDebug.Text += Environment.NewLine + "UIIMAGE NOT EXIST:" + Environment.NewLine + ImagePath;
 					}
-					else
-					{	
-					textDebug.Text += Environment.NewLine + "UIIMAGE NOT EXIST:"
-							+ Environment.NewLine + ImagePath;
-					}
-				} 
-				catch (Exception ex2)
-				{
-						textDebug.Text += Environment.NewLine + "FROM FILE: " 
-							+ Environment.NewLine + ImagePath 
-							+ Environment.NewLine + ex2.Message;
+				} catch (Exception ex2) {
+					textDebug.Text += Environment.NewLine + "FROM FILE: " + Environment.NewLine + ImagePath + Environment.NewLine + ex2.Message;
 				}
-				Console.WriteLine("gold " + goldBadges);
-				Console.WriteLine("silver " + silverBadges);
-				Console.WriteLine("bronze " + bronzeBadges);
+				Console.WriteLine ("gold " + goldBadges);
+				Console.WriteLine ("silver " + silverBadges);
+				Console.WriteLine ("bronze " + bronzeBadges);
 				
 				
 				labelPoints.Text = points;
-				string dateUpdated = "Last updated " + DateTime.Now.ToString("dd-MMM-yy hh:mm:ss");
+				string dateUpdated = "Last updated " + DateTime.Now.ToString ("dd-MMM-yy hh:mm:ss");
 				labelLastUpdated.Text = dateUpdated;
 				
-				string cache = String.Format("{0}?{1}?{2}?{3}?{4}?{5}?{6}"
-				                             , username
-				                             , points
-					                         , goldBadges
-					                         , silverBadges
-					                         , bronzeBadges
-				                             , profileUrl
-				                             , dateUpdated);
+				string cache = String.Format ("{0}?{1}?{2}?{3}?{4}?{5}?{6}", username, points, goldBadges, silverBadges, bronzeBadges, profileUrl, dateUpdated);
 				System.IO.File.WriteAllText (CachePath, cache);
-				labelConnectionError.Text = ""; // blank out if we get to here without Exception
-			} 
-			catch (Exception ex)
-			{
+				labelConnectionError.Text = "";
+				// blank out if we get to here without Exception
+			} catch (Exception ex) {
 				labelLastUpdated.Text = ex.Message;
-				textDebug.Text += Environment.NewLine
-					+ ex.Message;
+				textDebug.Text += Environment.NewLine + ex.Message;
 			}
 		}
-				
+
 		/// <summary>
 		/// Capture a copy of the current View and:
 		/// * re-display in a UIImage control
 		/// * save to the Photos collection
 		/// * save to disk in the application's Documents folder
 		/// </summary>
-		public void ScreenCapture()
+		public void ScreenCapture ()
 		{
-			Console.WriteLine("start image cap");
-			Console.WriteLine("frame" + this.View.Frame.Size);
-			UIGraphics.BeginImageContext(View.Frame.Size);
+			Console.WriteLine ("start image cap");
+			Console.WriteLine ("frame" + this.View.Frame.Size);
+			UIGraphics.BeginImageContext (View.Frame.Size);
 			var documentsDirectory = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
 			
-			var ctx = UIGraphics.GetCurrentContext();
-			if (ctx != null)
-			{
-				Console.WriteLine("ctx not null");
-				View.Layer.RenderInContext(ctx);
-				Console.WriteLine("render in context");
-				UIImage img = UIGraphics.GetImageFromCurrentImageContext();
-				Console.WriteLine("get from current content");
-				UIGraphics.EndImageContext();
+			var ctx = UIGraphics.GetCurrentContext ();
+			if (ctx != null) {
+				Console.WriteLine ("ctx not null");
+				View.Layer.RenderInContext (ctx);
+				Console.WriteLine ("render in context");
+				UIImage img = UIGraphics.GetImageFromCurrentImageContext ();
+				Console.WriteLine ("get from current content");
+				UIGraphics.EndImageContext ();
 				
 				// Set to display in a UIImage control _on_ the view
 				//imageLogo.Image = img;
 				
 				// Save to Photos
-				img.SaveToPhotosAlbum((sender, args)=>{
-					Console.WriteLine("image saved to Photos");
-				 	var av = new UIAlertView("Screenshot saved"
-	                                    , "Image saved to "+Environment.NewLine+"Photos : Camera Roll"
-	                                    , null
-	                                    , "Ok thanks"
-	                                    , null);
-	            		av.Show();
+				img.SaveToPhotosAlbum ((sender, args) =>
+				{
+					Console.WriteLine ("image saved to Photos");
+					var av = new UIAlertView ("Screenshot saved", "Image saved to " + Environment.NewLine + "Photos : Camera Roll", null, "Ok thanks", null);
+					av.Show ();
 				});
 				
 				// thought this "might" overwrite the splashscreen
@@ -377,18 +326,15 @@ namespace iSOFlair
 				// Save to application's Documents folder, kinda pointless except as an example
 				// since there is no way to "read" it
 				string png = Path.Combine (documentsDirectory, "Screenshot.png");
-				NSData imgData = img.AsPNG();
+				NSData imgData = img.AsPNG ();
 				NSError err = null;
-				if (imgData.Save(png, false, out err))
-				{
-					Console.WriteLine("saved " + png);
+				if (imgData.Save (png, false, out err)) {
+					Console.WriteLine ("saved " + png);
 				} else {
-					Console.WriteLine("not saved" + png + " because" + err.LocalizedDescription);
+					Console.WriteLine ("not saved" + png + " because" + err.LocalizedDescription);
 				}
-			}
-			else
-			{
-				Console.WriteLine("ctx null - doesn't happen but wasn't sure at first");
+			} else {
+				Console.WriteLine ("ctx null - doesn't happen but wasn't sure at first");
 			}
 		}
 	}
